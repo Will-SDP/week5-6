da79774d5090ce2bb2fbbbfcca0d3b34e1eada68
Will-SDP <William.e.cooper@student.shu.ac.uk>
2025-03-08 18:15:26 +0000

    Added js for menu control

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,14 @@
-var menu_button = document.getElementsByClassName('fa-bars')
-var menu = document.getElementById('menu')
+
+body = document.getElementsByTagName('body')
+var menu_visible = false
 
 function toggle_menu(){
     document.getElementById('menu').classList.toggle('hide')
+    menu_visible = !menu_visible
 }
+
+body[0].addEventListener("click", function(e){
+    if(e.pageX > 252 && menu_visible == true ){
+        toggle_menu()
+    }
+})
