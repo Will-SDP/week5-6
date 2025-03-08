5a9ff72c4e522356fe352c1dd5ecb5476b817ea9
Will-SDP <William.e.cooper@student.shu.ac.uk>
2025-03-08 17:41:40 +0000

    Added sidebar menu

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+var menu_button = document.getElementsByClassName('fa-bars')
+var menu = document.getElementById('menu')
+
+function toggle_menu(){
+    document.getElementById('menu').classList.toggle('hide')
+}
