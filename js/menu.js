04c0f4de8030e5ee101ef84b2334cd6eb7632e78
Will-SDP <William.e.cooper@student.shu.ac.uk>
2025-03-09 12:55:37 +0000

    Added accessablility to navigation

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 
 body = document.getElementsByTagName('body')
 var menu_visible = false
+menuBtns = document.querySelectorAll('.menu-btn')
 
 function toggle_menu(){
     document.getElementById('menu').classList.toggle('hide')
@@ -12,3 +13,12 @@ body[0].addEventListener("click", function(e){
         toggle_menu()
     }
 })
+
+menuBtns.forEach(element => {
+    element.addEventListener("keypress", function(e){
+        if(e.key == "Enter"){
+            toggle_menu()
+        }
+    })
+});
+
