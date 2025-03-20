08dcf9bad5f47ffec5e4afced613446c2e9e1b43
Will-SDP <William.e.cooper@student.shu.ac.uk>
2025-03-20 20:37:42 +0000

    Updated menu js and icon

--- js/menu.js
+++ js/menu.js
@@ -4,12 +4,13 @@ var menu_visible = false
 menuBtns = document.querySelectorAll('.menu-btn')
 
 function toggle_menu(){
-    document.getElementById('menu').classList.toggle('hide')
     menu_visible = !menu_visible
+    document.getElementById('menu').classList.toggle('hide')
+    
 }
 
 body[0].addEventListener("click", function(e){
-    if(e.pageX > 252 && menu_visible == true ){
+    if(e.pageX > 252 && menu_visible == true && e.target.tagName != 'I' ){
         toggle_menu()
     }
 })
