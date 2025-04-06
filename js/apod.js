36504a279431a3b42d0dc471ae4748347a28f2aa
Will-SDP <William.e.cooper@student.shu.ac.uk>
2025-04-06 21:29:28 +0100

    Added week 5-7 APOD

---
+++ js/apod.js
@@ -0,0 +1,21 @@
+const api_key = '7FrcB5NSylrQaCmOnIgM87y9jBBIbSgu1frAhrK9' 
+const url = 'https://api.nasa.gov/planetary/apod?api_key=7FrcB5NSylrQaCmOnIgM87y9jBBIbSgu1frAhrK9'
+document.getElementById('date').max = new Date().toISOString().split("T")[0];
+
+async function getImage(url) {
+    let result = await fetch(url);
+    let apod = await result.json();
+    document.getElementById('apod-image').src = apod.url
+    document.getElementById('apod-text').innerText = apod.explanation
+
+
+}
+
+document.getElementById('date').addEventListener('change', function(e){
+    newUrl = url + "&date=" + e.target.value
+    getImage(newUrl)
+})
+
+getImage(url)
+
+// https://stackoverflow.com/questions/32378590/set-date-input-fields-max-date-to-today
