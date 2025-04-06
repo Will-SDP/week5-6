20ad04e81b30bae3a0111e9abda60234db9fe150
Will-SDP <William.e.cooper@student.shu.ac.uk>
2025-04-06 21:50:32 +0100

    Set default date value

--- js/apod.js
+++ js/apod.js
@@ -1,5 +1,6 @@
 const api_key = '7FrcB5NSylrQaCmOnIgM87y9jBBIbSgu1frAhrK9' 
 const url = 'https://api.nasa.gov/planetary/apod?api_key=7FrcB5NSylrQaCmOnIgM87y9jBBIbSgu1frAhrK9'
+document.getElementById('date').value = new Date().toISOString().split("T")[0];
 document.getElementById('date').max = new Date().toISOString().split("T")[0];
 
 async function getImage(url) {
